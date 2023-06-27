4b1e512127eef2ec9fa9df0704964506abc8f40e
dylanmaupa <114060288+dylanmaupa@users.noreply.github.com>
2023-06-27 13:47:04 +0200

    #

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,52 +1,53 @@
 import { useState } from "react";
-import { Link, NavLink } from "react-router-dom"
-import { ReactComponent as Logo } from "../assets/images/resellme-logo.svg"
-import { ReactComponent as Sun } from "../assets/images/sun.svg"
-import { ReactComponent as Tabler } from "../assets/images/tabler_window.svg"
+import { Link, NavLink } from "react-router-dom";
+import { ReactComponent as Logo } from "../assets/images/resellme-logo.svg";
+import { ReactComponent as Sun } from "../assets/images/sun.svg";
+import { ReactComponent as Tabler } from "../assets/images/tabler_window.svg";
+import DarkMode from "./DarkMode/DarkMode";
 
 
 const NavBar = () => {
+    const [active, setActive] = useState(false);
 
-    const [active, setActive]= useState(false)
-
-  return (
-    <header>
-        <div className="nav">
-            <div className="logo">
-              <Link to='/'><Logo className="logo-img" /></Link>
+    return (
+        <header>
+            <div className="nav">
+                <div className="logo">
+                    <Link to='/'><Logo className="logo-img" /></Link>
+                </div>
+                <ul>
+                    <li><NavLink to="/">Docs</NavLink></li>
+                    <li><NavLink to="/">Blog</NavLink></li>
+                    <li><NavLink to="/">About Us</NavLink></li>
+                </ul>
             </div>
-            <ul>
-                <li><NavLink to="/">Docs</NavLink></li>
-                <li><NavLink to="/">Blog</NavLink></li>
-                <li><NavLink to="/">About Us</NavLink></li>
-            </ul>
-        </div>
-        <div className="nav2">
-            <ul>
-                <li>
-                    <NavLink to="/">Support<Tabler className="tabler"/></NavLink>
-                </li>
-                <li>
-                    <NavLink to="/">Portal<Tabler className="tabler"/></NavLink>
-                </li>
-                <li>
-                    <NavLink to="/">Register<Tabler className="tabler"/></NavLink>
-                </li>
-                <li><Sun/></li>
-                <li><input type="search" placeholder="Search" /></li>
-            </ul>
-        </div>
-        <div onClick={() => {setActive(!active)}} className={active ? "hamburger active":"hamburger"}>
-            <p>Menu</p>
-            <p className="lastP">Close</p>
-            <div className="menu">
-                <div className="line"></div>
-                <div className="line"></div>
-                <div className="line"></div>
+            <div className="nav2">
+                <ul>
+                    <li>
+                        <NavLink to="/">Support<Tabler className="tabler" /></NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/">Portal<Tabler className="tabler" /></NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/">Register<Tabler className="tabler" /></NavLink>
+                    </li>
+                    {/* <li><Sun onClick={toggleTheme} /></li> */}
+                    <DarkMode/>
+                    <li><input type="search" placeholder="Search" /></li>
+                </ul>
             </div>
-        </div>
-    </header>
-  )
-}
+            <div onClick={() => { setActive(!active); }} className={active ? "hamburger active" : "hamburger"}>
+                <p>Menu</p>
+                <p className="lastP">Close</p>
+                <div className="menu">
+                    <div className="line"></div>
+                    <div className="line"></div>
+                    <div className="line"></div>
+                </div>
+            </div>
+        </header>
+    );
+};
 
-export default NavBar
+export default NavBar;
--- src/components/PartnerCarousel.jsx
+++ src/components/PartnerCarousel.jsx
@@ -15,7 +15,7 @@ const PartnerCarousel = () => {
     const images = [img1,img2,img3,img4,img5,img6,img7,img8]
   return (
     <Swiper 
-    className='wrapper'
+    className='wrapper part-carousel'
     slidesPerView='auto'
     spaceBetween={30}
     >
